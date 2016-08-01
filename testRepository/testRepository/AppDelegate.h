b98628a431f40b68840e989dbba615d8aee71bb0
Huang <767106718@qq.con>
2016-08-01 12:13:47 +0800

    初始化项目

---
+++ testRepository/testRepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRepository
+//
+//  Created by 黄涛 on 16/8/1.
+//  Copyright © 2016年 huangtao.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testRepository/testRepository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRepository
+//
+//  Created by 黄涛 on 16/8/1.
+//  Copyright © 2016年 huangtao.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
